77fbe56173e6f7c4e57952c36b07ed3cc7bb68ef
dzakwandh <dzakwandh9@gmail.com>
2021-06-15 23:56:45 +0700

    ubah dikit halaman utama

--- Tubes-KPL/Tubes-KPL/HalamanUtama.Designer.cs
+++ Tubes-KPL/Tubes-KPL/HalamanUtama.Designer.cs
@@ -30,11 +30,11 @@ namespace Tubes_KPL
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label2 = new System.Windows.Forms.Label();
             this.btnDataPengguna = new System.Windows.Forms.Button();
             this.btnDataJasa = new System.Windows.Forms.Button();
             this.btnDataTransaksi = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,38 +42,31 @@ namespace Tubes_KPL
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(334, 369);
+            this.label1.Location = new System.Drawing.Point(394, 385);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(177, 24);
+            this.label1.Size = new System.Drawing.Size(221, 29);
             this.label1.TabIndex = 1;
             this.label1.Text = "FLASH LAUNDRY";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::Tubes_KPL.Properties.Resources.Asset_1;
-            this.pictureBox1.Location = new System.Drawing.Point(314, 173);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(225, 165);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label2.Location = new System.Drawing.Point(634, 425);
+            this.label2.Location = new System.Drawing.Point(845, 523);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(154, 16);
+            this.label2.Size = new System.Drawing.Size(187, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "Selamat datang kembali";
             // 
             // btnDataPengguna
             // 
-            this.btnDataPengguna.Location = new System.Drawing.Point(84, 63);
+            this.btnDataPengguna.Location = new System.Drawing.Point(207, 450);
+            this.btnDataPengguna.Margin = new System.Windows.Forms.Padding(4);
             this.btnDataPengguna.Name = "btnDataPengguna";
-            this.btnDataPengguna.Size = new System.Drawing.Size(125, 34);
+            this.btnDataPengguna.Size = new System.Drawing.Size(167, 42);
             this.btnDataPengguna.TabIndex = 4;
             this.btnDataPengguna.Text = "Data Pengguna";
             this.btnDataPengguna.UseVisualStyleBackColor = true;
@@ -81,9 +74,10 @@ namespace Tubes_KPL
             // 
             // btnDataJasa
             // 
-            this.btnDataJasa.Location = new System.Drawing.Point(369, 63);
+            this.btnDataJasa.Location = new System.Drawing.Point(421, 450);
+            this.btnDataJasa.Margin = new System.Windows.Forms.Padding(4);
             this.btnDataJasa.Name = "btnDataJasa";
-            this.btnDataJasa.Size = new System.Drawing.Size(125, 34);
+            this.btnDataJasa.Size = new System.Drawing.Size(167, 42);
             this.btnDataJasa.TabIndex = 5;
             this.btnDataJasa.Text = "Data Jasa";
             this.btnDataJasa.UseVisualStyleBackColor = true;
@@ -91,25 +85,38 @@ namespace Tubes_KPL
             // 
             // btnDataTransaksi
             // 
-            this.btnDataTransaksi.Location = new System.Drawing.Point(619, 63);
+            this.btnDataTransaksi.Location = new System.Drawing.Point(644, 450);
+            this.btnDataTransaksi.Margin = new System.Windows.Forms.Padding(4);
             this.btnDataTransaksi.Name = "btnDataTransaksi";
-            this.btnDataTransaksi.Size = new System.Drawing.Size(125, 34);
+            this.btnDataTransaksi.Size = new System.Drawing.Size(167, 42);
             this.btnDataTransaksi.TabIndex = 6;
             this.btnDataTransaksi.Text = "Data Transaksi";
             this.btnDataTransaksi.UseVisualStyleBackColor = true;
             this.btnDataTransaksi.Click += new System.EventHandler(this.btnDataTransaksi_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Tubes_KPL.Properties.Resources.Asset_1;
+            this.pictureBox1.Location = new System.Drawing.Point(354, 144);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 203);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
             // HalamanUtama
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.btnDataTransaksi);
             this.Controls.Add(this.btnDataJasa);
             this.Controls.Add(this.btnDataPengguna);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "HalamanUtama";
             this.Text = "Halaman Utama";
             this.Load += new System.EventHandler(this.HalamanUtama_Load);
