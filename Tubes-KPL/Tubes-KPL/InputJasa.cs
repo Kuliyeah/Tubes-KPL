85cd3c87b863b0d126214ac38bfdbf102dcbc8bb
dhanakbar <maulana261100@gmail.com>
2021-06-15 22:09:58 +0800

    Menambahkan inputJasa

---
+++ Tubes-KPL/Tubes-KPL/InputJasa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tubes_KPL
+{
+    public partial class InputJasa : Form
+    {
+        public InputJasa()
+        {
+            InitializeComponent();
+        }
+    }
+}
