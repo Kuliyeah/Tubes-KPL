191beaf970cfa24ebe04e4e980a63c8364db0bd0
dhanakbar <maulana261100@gmail.com>
2021-06-15 22:19:10 +0800

    InputJasa

--- Tubes-KPL/Tubes-KPL/InputJasa.Designer.cs
+++ Tubes-KPL/Tubes-KPL/InputJasa.Designer.cs
@@ -51,14 +51,14 @@ namespace Tubes_KPL
             // 
             this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(356, 75);
+            this.dataGridView1.Location = new System.Drawing.Point(356, 73);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(313, 183);
             this.dataGridView1.TabIndex = 22;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(501, 317);
+            this.button4.Location = new System.Drawing.Point(501, 315);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 20;
@@ -67,7 +67,7 @@ namespace Tubes_KPL
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(594, 317);
+            this.button3.Location = new System.Drawing.Point(594, 315);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 21;
@@ -76,14 +76,14 @@ namespace Tubes_KPL
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(101, 151);
+            this.textBox3.Location = new System.Drawing.Point(101, 149);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(212, 20);
             this.textBox3.TabIndex = 16;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(101, 188);
+            this.textBox4.Location = new System.Drawing.Point(101, 186);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(212, 70);
@@ -91,7 +91,7 @@ namespace Tubes_KPL
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(101, 112);
+            this.textBox2.Location = new System.Drawing.Point(101, 110);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(212, 20);
             this.textBox2.TabIndex = 18;
@@ -99,7 +99,7 @@ namespace Tubes_KPL
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(27, 154);
+            this.label4.Location = new System.Drawing.Point(27, 152);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(36, 13);
             this.label4.TabIndex = 11;
@@ -107,7 +107,7 @@ namespace Tubes_KPL
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(101, 75);
+            this.textBox1.Location = new System.Drawing.Point(101, 73);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(212, 20);
             this.textBox1.TabIndex = 19;
@@ -115,7 +115,7 @@ namespace Tubes_KPL
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(27, 191);
+            this.label5.Location = new System.Drawing.Point(27, 189);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(50, 13);
             this.label5.TabIndex = 12;
@@ -124,7 +124,7 @@ namespace Tubes_KPL
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(27, 115);
+            this.label3.Location = new System.Drawing.Point(27, 113);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(60, 13);
             this.label3.TabIndex = 13;
@@ -133,7 +133,7 @@ namespace Tubes_KPL
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(353, 59);
+            this.label6.Location = new System.Drawing.Point(353, 57);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(36, 13);
             this.label6.TabIndex = 14;
@@ -142,7 +142,7 @@ namespace Tubes_KPL
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(27, 78);
+            this.label2.Location = new System.Drawing.Point(27, 76);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(63, 13);
             this.label2.TabIndex = 15;
@@ -150,7 +150,7 @@ namespace Tubes_KPL
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(201, 317);
+            this.button2.Location = new System.Drawing.Point(201, 315);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 9;
@@ -159,7 +159,7 @@ namespace Tubes_KPL
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(97, 317);
+            this.button1.Location = new System.Drawing.Point(97, 315);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 10;
@@ -170,7 +170,7 @@ namespace Tubes_KPL
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(137, 34);
+            this.label1.Location = new System.Drawing.Point(137, 32);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(111, 20);
             this.label1.TabIndex = 8;
@@ -180,7 +180,7 @@ namespace Tubes_KPL
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(738, 391);
+            this.ClientSize = new System.Drawing.Size(714, 371);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
