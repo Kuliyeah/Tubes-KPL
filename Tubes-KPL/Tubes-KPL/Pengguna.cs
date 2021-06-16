71e31d0f548652b7a1a741d5d18fcfab3310bc49
lovanto <rifkylovanto@gmail.com>
2021-06-16 10:35:22 +0700

    set disabled pengguna

--- Tubes-KPL/Tubes-KPL/Pengguna.cs
+++ Tubes-KPL/Tubes-KPL/Pengguna.cs
@@ -9,11 +9,39 @@ namespace Tubes_KPL
             InitializeComponent();
         }
 
+        private void SetDisabled()
+        {
+            txtUsername.Enabled = false;
+            txtNoHP.Enabled = false;
+            txtAlamat.Enabled = false;
+            txtEmail.Enabled = false;
+            txtKataSandi.Enabled = false;
 
+            btnSave.Enabled = false;
+            btnUpdate.Enabled = false;
+            btnDelete.Enabled = false;
+            btnCancel.Enabled = false;
+            btnNew.Enabled = true;
+        }
+
+        private void SetEnabled()
+        {
+            txtUsername.Enabled = true;
+            txtNoHP.Enabled = true;
+            txtAlamat.Enabled = true;
+            txtEmail.Enabled = true;
+            txtKataSandi.Enabled = true;
+
+            btnSave.Enabled = true;
+            btnUpdate.Enabled = true;
+            btnDelete.Enabled = true;
+            btnCancel.Enabled = true;
+            btnNew.Enabled = false;
+        }
 
         private void Pengguna_Load(object sender, System.EventArgs e)
         {
-
+            SetDisabled();
         }
 
         private void btnSave_Click(object sender, System.EventArgs e)
