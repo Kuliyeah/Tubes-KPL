770be93b13acebbcc6072f18c66c33d8bf2f68ed
Gilang Gumelar <58198142+gilgumlar@users.noreply.github.com>
2021-06-17 14:57:48 +0700

    Update InputTransaksi.cs

--- Tubes-KPL/Tubes-KPL/InputTransaksi.cs
+++ Tubes-KPL/Tubes-KPL/InputTransaksi.cs
@@ -7,17 +7,16 @@ namespace Tubes_KPL
 {
     public partial class InputTransaksi : Form
     {
+        List<InputTransaksiModel> transaksi = new List<InputTransaksiModel>();
         public InputTransaksi()
         {
             InitializeComponent();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
             setEditEnabled(false);
             btnNew.Enabled = true;
         }
-
         private void setEditEnabled(bool stat)
         {
             btnSimpan.Enabled = stat;
@@ -30,23 +29,21 @@ namespace Tubes_KPL
             textOngkir.Enabled = stat;
             textTotal.Enabled = stat;
         }
-
         private void clearText()
         {
             textIDTransaksi.Text = "";
-            textBerat.Text= "";
+            textBerat.Text = "";
             textOngkir.Text = "";
             textTotal.Text = "";
             textDeskripsi.Text = "";
+            comboBoxIDJasa.Text = "";
         }
-
         private int hitungTotal()
         {
             int harga = 5000;
             int total = Int16.Parse(textBerat.Text) * harga + Int16.Parse(textOngkir.Text);
             return total;
         }
-
         private void btnNew_Click(object sender, EventArgs e)
         {
             clearText();
@@ -59,7 +56,39 @@ namespace Tubes_KPL
         {
             setEditEnabled(false);
             btnNew.Enabled = true;
+
+            int idTransaksi = Int16.Parse(textIDTransaksi.Text);
+            int idJasa = Int16.Parse(comboBoxIDJasa.Text);
+            String deskripsi = textDeskripsi.Text;
+            int berat = Int16.Parse(textBerat.Text);
+            int ongkir = Int16.Parse(textOngkir.Text);
+            int totalBayar = hitungTotal();
             textTotal.Text = hitungTotal().ToString();
+
+
+            InputTransaksiModel dataTransaksi = new InputTransaksiModel(idTransaksi, idJasa, deskripsi,
+                berat, ongkir, totalBayar);
+            transaksi.Add(dataTransaksi);
+
+            DataTable dtTransaski = new DataTable();
+            dtTransaski.Columns.Add("ID Transaksi");
+            dtTransaski.Columns.Add("ID Jasa");
+            dtTransaski.Columns.Add("Deskripsi");
+            dtTransaski.Columns.Add("Berat");
+            dtTransaski.Columns.Add("Ongkir");
+            dtTransaski.Columns.Add("Total Bayar");
+
+            for (int i = 0; i < transaksi.Count; i++)
+            {
+                dtTransaski.Rows.Add(
+                    transaksi[i].getIdTransaksi().ToString(),
+                    transaksi[i].getIdJasa().ToString(),
+                    transaksi[i].getDeskripsiCucian().ToString(),
+                    transaksi[i].getBeratCucian().ToString(),
+                    transaksi[i].getOngkir().ToString(),
+                    transaksi[i].getTotalBayar().ToString()
+                    );
+            }
         }
 
         private void btnBatal_Click(object sender, EventArgs e)
