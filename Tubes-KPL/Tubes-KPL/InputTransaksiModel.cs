3a85cfaef1f46b690b3433ea323b4a3542b1e03e
Gilang Gumelar <58198142+gilgumlar@users.noreply.github.com>
2021-06-17 14:57:15 +0700

    Update InputTransaksi

---
+++ Tubes-KPL/Tubes-KPL/InputTransaksiModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tubes_KPL
+{
+    class InputTransaksiModel
+    {//deklarasi variable
+        private String _deskripsiCucian;
+        private int _idTransaksi, _idJasa, _totalBayar, _beratCucian, _ongkir;
+
+        //constructor: membuat data transaksi baru
+        public InputTransaksiModel(int idTransaksi, int idJasa,
+            String deskripsiCucian, int beratCucian, int ongkir, int totalBayar)
+        {
+            this._idTransaksi = idTransaksi;
+            this._idJasa = idJasa;
+            this._deskripsiCucian = deskripsiCucian;
+            this._beratCucian = beratCucian;
+            this._ongkir = ongkir;
+            this._totalBayar = totalBayar;
+        }
+
+        //(getter)ambil value dari idtoko
+        public int getIdTransaksi()
+        {
+            return _idTransaksi;
+        }
+
+        //Dzakwan Daffa Hidayatullah - 1302194008
+        //(getter) ambil value dari idJasa
+        public int getIdJasa()
+        {
+            return _idJasa;
+        }
+
+        //(getter) ambil value dari deskripsiCucian
+        public String getDeskripsiCucian()
+        {
+            return _deskripsiCucian;
+        }
+
+        //(getter) ambil value dari _beratCucian
+        public int getBeratCucian()
+        {
+            return _beratCucian;
+        }
+
+        //(getter) ambil value dari ongkir
+        public int getOngkir()
+        {
+            return _ongkir;
+        }
+
+        //(getter) ambil value dari totalBayar
+        public int getTotalBayar()
+        {
+            return _totalBayar;
+        }
+
+        //(setter) setting idTransaksi
+        public void setIdTransaksi(int idTransaksi)
+        {
+            this._idTransaksi = idTransaksi;
+        }
+
+        //(setter) setting idJasa
+        public void setIdJasa(int idJasa)
+        {
+            this._idJasa = idJasa;
+        }
+
+        //Dzakwan Daffa Hidayatullah - 1302194008
+        //(setter) setting deskripsiCucian
+        public void setDeskripsiCucian(String deskripsiCucian)
+        {
+            this._deskripsiCucian = deskripsiCucian;
+        }
+
+        //(setter) setting beratCucian
+        public void setBeratCucian(int beratCucian)
+        {
+            this._beratCucian = beratCucian;
+        }
+
+        //(setter) setting ongkir
+        public void setOngkir(int ongkir)
+        {
+            this._ongkir = ongkir;
+        }
+
+        //(setter) setting idTotalBayar
+        public void setTotalBayar(int totalBayar)
+        {
+            this._totalBayar = totalBayar;
+        }
+    }
+}
