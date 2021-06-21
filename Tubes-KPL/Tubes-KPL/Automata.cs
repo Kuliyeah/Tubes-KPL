0773ea30ae6ae3b41e5711c63179366f61591138
adamnn26 <muhamadadam@student.telkomuniversity.ac.id>
2021-06-21 22:39:05 +0700

    Menambahkan Class Automata

---
+++ Tubes-KPL/Tubes-KPL/Automata.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Tubes_KPL
+{
+    class Automata
+    {
+        string[] State = { "Login", "Dashboard", "InputPengguna", "InputJasa", "InputTransaksi", "Logout" };
+        string posisi;
+
+        public Automata() { }
+        public void setPosisi(string pos)
+        {
+            this.posisi = pos;
+        }
+
+        public string getPosisi()
+        {
+            return this.posisi;
+        }
+
+        public void posisiController(string btn)
+        {
+            if (this.posisi == "Login")
+            {
+                if (btn == "Login")
+                    new Dashboard().Show();
+
+            }
+            else if (this.posisi == "Dashboard")
+            {
+                if (btn == "Data Pengguna")
+                    new Pengguna().Show();
+                else if (btn == "Data Jasa")
+                    new InputJasa().Show();
+                else if (btn == "Data Transaksi")
+                    new InputTransaksi().Show();
+                else if (btn == "Logout")
+                    new Login().Show();
+            }
+        }
+    }
+}
