7659d98395f93af37eb452cc433de0eb4113f3f8
Gilang Gumelar <58198142+gilgumlar@users.noreply.github.com>
2021-06-27 19:14:24 +0700

    Update Komen Config.cs

--- Tubes-KPL/Tubes-KPL/Config.cs
+++ Tubes-KPL/Tubes-KPL/Config.cs
@@ -5,6 +5,8 @@ namespace Tubes_KPL
 {
     class Config
     {
+        // Membaca file JSON dari path yang ada di parameter
+        // dan me-return data pada file tersebut sesuai dengan tipe generik yang dimasukan.
         public static T ReadFromJson<T>(string path)
         {
             string json = File.ReadAllText(path);
@@ -12,6 +14,7 @@ namespace Tubes_KPL
             return obj;
         }
 
+        // Menyimpan file JSON ke lokasi path yang ada di parameter dengan tipe generik yang dimasukan.
         public static void SaveToJson<T>(T obj, string path)
         {
             string json = JsonConvert.SerializeObject(obj, Formatting.Indented);
