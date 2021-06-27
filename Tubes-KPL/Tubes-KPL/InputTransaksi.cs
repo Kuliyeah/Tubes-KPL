10d868787e85f60a9577abfaac85b96e2bbc500b
Gilang Gumelar <58198142+gilgumlar@users.noreply.github.com>
2021-06-27 19:06:26 +0700

    Update Komen di InputTransaksi.cs

--- Tubes-KPL/Tubes-KPL/InputTransaksi.cs
+++ Tubes-KPL/Tubes-KPL/InputTransaksi.cs
@@ -6,12 +6,13 @@ using System.Windows.Forms;
 namespace Tubes_KPL
 {
     public partial class InputTransaksi : Form
-    {       
+    {
+        // Init var.
         DataTable dtTransaksi = new DataTable(), dtJasa;
         DateTime tglSekarang = DateTime.Now;
         private string pathDir = Environment.CurrentDirectory;
         private string pathTransaksi = @"../../../json/InputTransaksi.json";
-        private string pathJasa= @"../../../json/InputJasa.json";
+        private string pathJasa = @"../../../json/InputJasa.json";
         private string pathMoney = @"../../../json/MoneyConfig.json";
         private moneyConfig money;
         Automata.State posisi = Automata.State.INPUT_TRANSAKSI, nextPosisi;
@@ -20,22 +21,25 @@ namespace Tubes_KPL
         {
             InitializeComponent();
 
-            try //jika file JSON sudah ada maka akan membaca data tersebut
+            // Membaca file InputTransaksi.json.
+            try
             {
                 dtTransaksi = Config.ReadFromJson<DataTable>(pathDir + pathTransaksi);
             }
-            catch //jika file JSON belum ada maka akan membuat file JSON
+            // Membuat file InputTransaksi.json jika file tersebut belum tersedia.
+            catch
             {
-                //membuat tabel data
+                // Set dummy data.
                 DummyData();
 
                 Config.SaveToJson<DataTable>(dtTransaksi, pathDir + pathTransaksi);
             }
 
-            //tampilkan data dari InputTransaksi.json
+            // Tampilkan data dari InputTransaksi.json
             dgvTransaksi.DataSource = dtTransaksi;
         }
 
+        // Membuat Data palsu untuk JSON.
         private void DummyData()
         {
             dtTransaksi.Columns.Add("Tanggal");
@@ -48,6 +52,7 @@ namespace Tubes_KPL
             dtTransaksi.Rows.Add(DateTime.Now, "1234", "Test Jasa", "Deskripsi Test Jasa", "2", "5000", "15000");
         }
 
+        // Method yang dijalankan ketika InputTransaksi.cs dalam posisi show.
         private void Form1_Load(object sender, EventArgs e)
         {
             setEditEnabled(false);
@@ -55,7 +60,10 @@ namespace Tubes_KPL
             tbTanggal.Text = tglSekarang.ToString();
             setComboboxNamaJasa();
 
+            // Membaca file MoneyConfig.json dan menyimpan ke var money.
             money = Config.ReadFromJson<moneyConfig>(pathDir + pathMoney);
+
+            // Set tampilan text pada LbMoney.
             if (money.getMoneyConfig() == "Rupiah")
             {
                 LbMoney.Text = "Mata Uang : Rupiah";
@@ -67,6 +75,7 @@ namespace Tubes_KPL
             convertMataUang();
         }
 
+        // Mengaktifkan/Menonaktifkan sebagian tombol dan text box.
         private void setEditEnabled(bool stat)
         {
             btnSimpan.Enabled = stat;
@@ -81,6 +90,7 @@ namespace Tubes_KPL
             tbTanggal.Enabled = false;
         }
 
+        // Untuk me-reset text yang sudah diinputkan pada text box.
         private void clearText()
         {
             tbIDTransaksi.Text = "";
@@ -91,28 +101,34 @@ namespace Tubes_KPL
             cbNamaJasa.Text = "";
         }
 
+        // Untuk mencari harga jasa.
         private int cariHargaJasa()
         {
+            // Membaca file InputJasa.json dan me-return harga.
             dtJasa = Config.ReadFromJson<DataTable>(pathDir + pathJasa);
             int harga = Int32.Parse(dtJasa.Rows[cbNamaJasa.SelectedIndex][2].ToString());
             return harga;
         }
 
+        // Untuk menghitung total bayar.
         private int hitungTotal(int berat, int harga, int ongkir)
         {
             int total = berat * harga + ongkir;
             return total;
         }
 
+        // Set data combobox nama jasa.
         private void setComboboxNamaJasa()
         {
-            try //jika file JSON sudah ada maka akan membaca data tersebut
+            // Membaca file InputJasa.json.
+            try
             {
                 dtJasa = Config.ReadFromJson<DataTable>(pathDir + pathJasa);
             }
-            catch //jika file JSON belum ada maka akan membuat file JSON
+            // Membuat file InputJasa.json jika file tersebut belum tersedia.
+            catch
             {
-                //membuat tabel data
+                // Membuat tabel data
                 dtJasa = new DataTable();
                 dtJasa.Columns.Add("Nama Toko");
                 dtJasa.Columns.Add("Nama Jasa");
@@ -121,10 +137,13 @@ namespace Tubes_KPL
                 dtJasa.Columns.Add("Deskripsi Jasa");
                 Config.SaveToJson<DataTable>(dtJasa, pathDir + pathJasa);
             }
+
+            // Set data combobox nama jasa.
             cbNamaJasa.DataSource = dtJasa;
             cbNamaJasa.DisplayMember = "Nama Jasa";
         }
 
+        // Ketika btnNew di klik, maka akan memanggil beberapa method dan menonaktifkan sebagian tombol dan text box.
         private void btnNew_Click(object sender, EventArgs e)
         {
             clearText();
@@ -133,15 +152,17 @@ namespace Tubes_KPL
             tbTotal.Enabled = false;
         }
 
+        // Ketika btnSimpan di klik, akan menyimpan data yang diinput kedalam dgvTransaksi.
         private void btnSimpan_Click(object sender, EventArgs e)
         {
+            // Membuat List.
             List<InputTransaksiModel> transaksi = new List<InputTransaksiModel>();
 
-            //set button
+            // Set button dan text box.
             setEditEnabled(false);
             btnNew.Enabled = true;
 
-            //set dan ambil nilai dari input user
+            // Set dan ambil nilai dari input user.
             int idTransaksi = Int32.Parse(tbIDTransaksi.Text);
             String namaJasa = cbNamaJasa.Text;
             String deskripsi = tbDeskripsi.Text;
@@ -150,11 +171,11 @@ namespace Tubes_KPL
             int totalBayar = hitungTotal(berat, cariHargaJasa(), ongkir);
             tbTotal.Text = totalBayar.ToString();
 
-            //masukan data kedalam list
+            // Memasukan data kedalam list.
             transaksi.Add(new InputTransaksiModel(tglSekarang, idTransaksi, namaJasa, deskripsi,
                 berat, ongkir, totalBayar));
 
-            //isi tabel data dengan data dari list
+            // Mengisi tabel data dengan data dari list.
             for (int i = 0; i < transaksi.Count; i++)
             {
                 dtTransaksi.Rows.Add(
@@ -168,16 +189,18 @@ namespace Tubes_KPL
                     );
             }
 
-            //tampilkan data ke UI
+            // Menampilkan data ke UI.
             dgvTransaksi.DataSource = dtTransaksi;
 
-            //simpan dan update data ke JSON
+            // Simpan dan update data ke file InputTransaksi.json.
             Config.SaveToJson<DataTable>(dtTransaksi, pathDir + pathTransaksi);
         }
 
+        // Ketika btnBatal di klik, maka mengosongkan semua textBox, menonaktifkan sebagian tombol,
+        // lalu menutup UI InputTransaksi dan menampikan UI dashboard.
         private void btnBatal_Click(object sender, EventArgs e)
         {
-            clearText(); 
+            clearText();
             setEditEnabled(false);
             btnNew.Enabled = true;
             this.Hide();
@@ -187,12 +210,16 @@ namespace Tubes_KPL
             Automata.posisiTransition(nextPosisi);
         }
 
+        // Untuk mengubah mata uang dari Rupiah ke USD dan sebaliknya.
         public void convertMataUang()
         {
+            // Membaca file MoneyConfig.json.
             money = Config.ReadFromJson<moneyConfig>(pathDir + pathMoney);
+
+            // Merubah data ongkir dan total dari Rupiah ke USD atau sebaliknya.
             if (money.getMoneyConfig() == "USD")
             {
-                for (int i = 0; i < dgvTransaksi.RowCount-1; i++)
+                for (int i = 0; i < dgvTransaksi.RowCount - 1; i++)
                 {
                     dgvTransaksi.Rows[i].Cells[5].Value = (Double.Parse(dgvTransaksi.Rows[0 + i].Cells[5].Value.ToString()) / 14000).ToString().Substring(0, 4);
                     dgvTransaksi.Rows[i].Cells[6].Value = (Double.Parse(dgvTransaksi.Rows[0 + i].Cells[6].Value.ToString()) / 14000).ToString().Substring(0, 4);
